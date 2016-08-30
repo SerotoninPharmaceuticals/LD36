e18d9a9ab23e8a5ba22c34046a260d0bb88ae2a2
serotoninphobe <gray_hawks@hotmail.com>
2016-08-30 22:04:33 +0800

    标题修改

--- source/IntroState.hx
+++ source/IntroState.hx
@@ -32,7 +32,7 @@ class IntroState extends FlxState {
   }
 
   private function createTimerBar():Void {
-    var timerBar = new TimerBar(-50, 100, 0, 0.6);
+    var timerBar = new TimerBar(-50, 90, 0, 0.6);
     timerBar.start();
     add(timerBar);
   }
@@ -40,9 +40,9 @@ class IntroState extends FlxState {
   private function createText():Void {
     var titleText = new FlxText(0, 120, 800, "A FUNERAL");
     titleText.setFormat("assets/fonts/Avera-Sans-Cn-Lt.ttf", 120, FlxColor.WHITE, CENTER);
-    var text0 = new FlxText(0, 350, 800, "A short game for Ludum Dare 36");
+    var text0 = new FlxText(0, 285, 800, "A Short Game for Ludum Dare 36");
     text0.setFormat("assets/fonts/Avera-Sans-Cn-Lt.ttf", 24, FlxColor.WHITE, CENTER);
-    var text1 = new FlxText(0, 380, 800, "click anywhere to start");
+    var text1 = new FlxText(0, 315, 800, "- click anywhere to start -");
     text1.setFormat("assets/fonts/Avera-Sans-Cn-Lt.ttf", 18, FlxColor.WHITE, CENTER);
     add(titleText);
     add(text0);
