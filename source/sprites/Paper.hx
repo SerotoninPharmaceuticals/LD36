b431cd9cc5e1da4dc3bc4f1f241ef1fb94e7a112
serotoninphobe <gray_hawks@hotmail.com>
2016-09-22 22:36:51 +0800

    添加电话音效

--- source/sprites/Paper.hx
+++ source/sprites/Paper.hx
@@ -15,16 +15,17 @@ class Paper extends FlxTypedGroup<FlxSprite> {
   var hitbox:FlxSprite;
 
   var largeImage:String;
+  var indexName:String;
   var paperSound:FlxSound;
+  var phoneSound:FlxSound;
 
   var hover = false;
-  public var indexName:String;
   private var iniOn:Bool;
 
   public function new(X:Float = 0, Y:Float = 0, name:String, _onOpen:FlxSprite->(Void->Void)->Void, startOn:Bool = false) {
     super();
-    indexName = name;
     iniOn = startOn;
+    indexName = name;
     paper = new FlxSprite(X, Y);
     var image:String = GameConfig.IMAGE_PATH + name + '_small.png';
 
@@ -41,6 +42,7 @@ class Paper extends FlxTypedGroup<FlxSprite> {
     onOpen = _onOpen;
 
     paperSound = FlxG.sound.load("assets/sounds/paper.wav", 0.6, false);
+    phoneSound = FlxG.sound.load("assets/sounds/phone.wav", 0.5, false);
 
     add(paper);
   }
@@ -66,7 +68,7 @@ class Paper extends FlxTypedGroup<FlxSprite> {
   function handleClick() {
     if(iniOn) {
       var largePaper = new FlxSprite(50, 0);
-      paperSound.play();
+      phoneSound.play();
       largePaper.loadGraphic(GameConfig.IMAGE_PATH + "phone.png");
 	  
       paper.kill();
@@ -82,7 +84,8 @@ class Paper extends FlxTypedGroup<FlxSprite> {
     if (FlxG.mouse.justPressed && !opened) {
       if (FlxCollision.pixelPerfectPointCheck(Std.int(FlxG.mouse.x), Std.int(FlxG.mouse.y), hitbox)) {
         var largePaper = new FlxSprite(50, 0);
-        paperSound.play();
+        if (indexName == "phone") phoneSound.play();
+        else paperSound.play();
         largePaper.loadGraphic(largeImage);
 
         paper.kill();
